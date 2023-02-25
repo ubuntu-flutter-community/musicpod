63c5c1f7f082c155087b7c7b0ba8da3679fb6a38
Feichtmeier <frederik.feichtmeier@gmail.com>
2023-02-25 15:35:17 +0100

    MD layout

--- lib/app/music_app.dart
+++ lib/app/music_app.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:music/l10n/l10n.dart';
 import 'package:yaru/yaru.dart';
 import 'package:yaru_icons/yaru_icons.dart';
 import 'package:yaru_widgets/yaru_widgets.dart';
+
+import 'package:music/l10n/l10n.dart';
 
 class MusicApp extends StatelessWidget {
   const MusicApp({super.key});
@@ -39,85 +40,153 @@ class _HomeState extends State<Home> {
 
   @override
   Widget build(BuildContext context) {
-    return YaruNavigationPage(
-      length: 3,
-      leading: const SizedBox(
-        width: 60,
-        child: YaruWindowTitleBar(style: YaruTitleBarStyle.undecorated),
+    final masterItems = [
+      MasterItem(
+        tileBuilder: (context) {
+          return const Text('Home');
+        },
+        builder: (context) {
+          return const Center(
+            child: Text('Home'),
+          );
+        },
+        iconBuilder: (context, selected) {
+          return selected
+              ? const Icon(YaruIcons.home_filled)
+              : const Icon(YaruIcons.home);
+        },
       ),
-      itemBuilder: (context, index, selected) {
-        switch (index) {
-          case 0:
-            return YaruNavigationRailItem(
-              icon: const Icon(YaruIcons.headphones),
-              style: YaruNavigationRailStyle.compact,
-            );
-          case 1:
-            return YaruNavigationRailItem(
-              icon: const Icon(YaruIcons.modem),
-              style: YaruNavigationRailStyle.compact,
-            );
-          default:
-            return YaruNavigationRailItem(
-              icon: const Icon(YaruIcons.network_cellular),
-              style: YaruNavigationRailStyle.compact,
-            );
-        }
-      },
-      pageBuilder: (context, index) {
-        Widget body = const Center(
-          child: YaruCircularProgressIndicator(),
-        );
+      MasterItem(
+        tileBuilder: (context) {
+          return const Text('Library');
+        },
+        builder: (context) {
+          return const Center(
+            child: Text('Library'),
+          );
+        },
+        iconBuilder: (context, selected) {
+          return selected
+              ? const Icon(YaruIcons.book_filled)
+              : const Icon(YaruIcons.book);
+        },
+      ),
+      MasterItem(
+        tileBuilder: (context) {
+          return const Text('JP doggo walk playlist');
+        },
+        builder: (context) {
+          return const Center(
+            child: Text('JP doggo walk playlist'),
+          );
+        },
+      ),
+    ];
 
-        switch (index) {
-          case 0:
-            body = Center(
-              child: Text(context.l10n.music),
-            );
-            break;
-          case 1:
-            body = Center(
-              child: Text(context.l10n.radio),
-            );
-            break;
-          default:
-            body = Center(
-              child: Text(context.l10n.podcasts),
-            );
-            break;
-        }
+    final appBar = YaruWindowTitleBar(
+      leading: Center(
+        child: YaruIconButton(
+          isSelected: _searchActive,
+          icon: const Icon(YaruIcons.search),
+          onPressed: () => setState(() {
+            _searchActive = !_searchActive;
+          }),
+        ),
+      ),
+      title: _searchActive
+          ? const SizedBox(
+              height: 35,
+              // width: 400,
+              child: TextField(),
+            )
+          : const Text('Ubuntu Music'),
+    );
 
-        return YaruDetailPage(
-          appBar: YaruWindowTitleBar(
-            leading: Center(
-              child: YaruIconButton(
-                isSelected: _searchActive,
-                icon: const Icon(YaruIcons.search),
-                onPressed: () => setState(() {
-                  _searchActive = !_searchActive;
-                }),
-              ),
+    final audioPlayerBottom = SizedBox(
+      height: 60,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: const [
+          Expanded(
+            child: YaruIconButton(
+              icon: Icon(YaruIcons.skip_backward),
             ),
-            title: _searchActive
-                ? const SizedBox(
-                    height: 35,
-                    width: 400,
-                    child: Padding(
-                      padding: EdgeInsets.only(right: 40),
-                      child: TextField(),
-                    ),
-                  )
-                : Text(
-                    index == 0
-                        ? context.l10n.music
-                        : index == 1
-                            ? context.l10n.radio
-                            : context.l10n.podcasts,
-                  ),
           ),
-          body: body,
-        );
-      },
+          YaruIconButton(
+            icon: Icon(YaruIcons.media_play),
+          ),
+          Expanded(
+            child: YaruIconButton(
+              icon: Icon(YaruIcons.skip_forward),
+            ),
+          ),
+        ],
+      ),
+    );
+
+    return Column(
+      children: [
+        appBar,
+        Expanded(
+          child: YaruMasterDetailPage(
+            layoutDelegate: const YaruMasterResizablePaneDelegate(
+              initialPaneWidth: 280,
+              minPaneWidth: 170,
+              minPageWidth: kYaruMasterDetailBreakpoint / 2,
+            ),
+            length: masterItems.length,
+            initialIndex: 0,
+            tileBuilder: (context, index, selected) {
+              final tile = YaruMasterTile(
+                title: masterItems[index].tileBuilder(context),
+                leading: masterItems[index].iconBuilder == null
+                    ? null
+                    : masterItems[index].iconBuilder!(context, selected),
+              );
+
+              Widget? column;
+
+              if (index == 2) {
+                column = Column(
+                  children: [
+                    const Divider(
+                      height: 30,
+                    ),
+                    tile
+                  ],
+                );
+              }
+
+              return column ??
+                  YaruMasterTile(
+                    title: masterItems[index].tileBuilder(context),
+                    leading: masterItems[index].iconBuilder == null
+                        ? null
+                        : masterItems[index].iconBuilder!(context, selected),
+                  );
+            },
+            pageBuilder: (context, index) => YaruDetailPage(
+              body: masterItems[index].builder(context),
+            ),
+          ),
+        ),
+        const Divider(
+          height: 0,
+        ),
+        audioPlayerBottom
+      ],
     );
   }
+}
+
+class MasterItem {
+  MasterItem({
+    required this.tileBuilder,
+    required this.builder,
+    this.iconBuilder,
+  });
+
+  final WidgetBuilder tileBuilder;
+  final WidgetBuilder builder;
+  final Widget Function(BuildContext context, bool selected)? iconBuilder;
 }
