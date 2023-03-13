74f8e55b689f232a4d134b192f687ebacfb5a38a
Feichtmeier <frederik.feichtmeier@gmail.com>
2023-03-13 16:11:24 +0100

    Also search podcast in isolate

--- lib/app/podcasts/podcast_model.dart
+++ lib/app/podcasts/podcast_model.dart
@@ -1,7 +1,6 @@
 import 'dart:async';
 
 import 'package:collection/collection.dart';
-import 'package:metadata_god/metadata_god.dart';
 import 'package:musicpod/data/audio.dart';
 import 'package:musicpod/data/countries.dart';
 import 'package:musicpod/data/podcast_genre.dart';
@@ -10,18 +9,15 @@ import 'package:podcast_search/podcast_search.dart';
 import 'package:safe_change_notifier/safe_change_notifier.dart';
 
 class PodcastModel extends SafeChangeNotifier {
+  PodcastModel(this._service);
+
   final PodcastService _service;
-  PodcastModel(this._service) : _search = Search();
+
   StreamSubscription<bool>? _chartsChangedSub;
+  StreamSubscription<bool>? _searchChangedSub;
 
-  final Search _search;
-
-  Set<Set<Audio>>? _podcastSearchResult;
-  Set<Set<Audio>>? get podcastSearchResult => _podcastSearchResult;
-  set podcastSearchResult(Set<Set<Audio>>? value) {
-    _podcastSearchResult = value;
-    notifyListeners();
-  }
+  Set<Set<Audio>>? get chartsPodcasts => _service.chartsPodcasts;
+  Set<Set<Audio>>? get podcastSearchResult => _service.searchResult;
 
   String? _searchQuery;
   String? get searchQuery => _searchQuery;
@@ -30,8 +26,6 @@ class PodcastModel extends SafeChangeNotifier {
     _searchQuery = value;
     notifyListeners();
   }
-
-  Set<Set<Audio>>? get chartsPodcasts => _service.chartsPodcasts;
 
   Country _country = Country.UNITED_STATES;
   Country get country => _country;
@@ -81,6 +75,9 @@ class PodcastModel extends SafeChangeNotifier {
     _chartsChangedSub = _service.chartsChanged.listen((_) {
       notifyListeners();
     });
+    _searchChangedSub = _service.searchChanged.listen((_) {
+      notifyListeners();
+    });
     if (_service.chartsPodcasts?.isNotEmpty == true) return;
     final c = countries.firstWhereOrNull((c) => c.countryCode == countryCode);
     if (c != null) {
@@ -92,68 +89,21 @@ class PodcastModel extends SafeChangeNotifier {
   @override
   void dispose() {
     _chartsChangedSub?.cancel();
+    _searchChangedSub?.cancel();
     super.dispose();
   }
 
   void loadCharts() =>
       _service.loadCharts(podcastGenre: podcastGenre, country: country);
 
-  Future<void> search({String? searchQuery, bool useAlbumImage = false}) async {
-    if (searchQuery?.isEmpty == true) return;
-    podcastSearchResult = null;
-
-    SearchResult results = await _search.search(
-      searchQuery!,
-      country: _country,
-      language: _language,
-      limit: 10,
-    );
-
-    if (results.successful && results.items.isNotEmpty) {
-      _podcastSearchResult ??= {};
-      _podcastSearchResult?.clear();
-
-      for (var item in results.items) {
-        if (item.feedUrl != null) {
-          try {
-            final Podcast podcast = await Podcast.loadFeed(
-              url: item.feedUrl!,
-            );
-
-            if (podcast.episodes?.isNotEmpty == true) {
-              final episodes = <Audio>{};
-
-              for (var episode in podcast.episodes!) {
-                if (episode.contentUrl != null) {
-                  final audio = Audio(
-                    url: episode.contentUrl,
-                    audioType: AudioType.podcast,
-                    name: '${podcast.title ?? ''} - ${episode.title}',
-                    imageUrl: useAlbumImage
-                        ? podcast.image ?? episode.imageUrl
-                        : episode.imageUrl ?? podcast.image,
-                    metadata: Metadata(
-                      title: episode.title,
-                      album: podcast.title,
-                      artist: podcast.copyright,
-                    ),
-                    description: podcast.description,
-                    website: podcast.url,
-                  );
-
-                  episodes.add(audio);
-                }
-              }
-              _podcastSearchResult?.add(episodes);
-              notifyListeners();
-            }
-          } on PodcastFailedException {
-            notifyListeners();
-          }
-        }
-      }
-    } else {
-      podcastSearchResult = {};
-    }
-  }
+  Future<void> search({
+    String? searchQuery,
+    bool useAlbumImage = false,
+  }) async =>
+      _service.search(
+        language: _language,
+        searchQuery: searchQuery,
+        country: _country,
+        useAlbumImage: useAlbumImage,
+      );
 }
--- lib/service/podcast_service.dart
+++ lib/service/podcast_service.dart
@@ -7,10 +7,13 @@ import 'package:musicpod/data/podcast_genre.dart';
 import 'package:podcast_search/podcast_search.dart';
 
 class PodcastService {
+  PodcastService() : _search = Search();
+
   final _chartsChangedController = StreamController<bool>.broadcast();
   Stream<bool> get chartsChanged => _chartsChangedController.stream;
   Set<Set<Audio>>? _chartsPodcasts;
   Set<Set<Audio>>? get chartsPodcasts => _chartsPodcasts;
+  final Search _search;
 
   static Future<Podcast> loadPodcast(String url) async =>
       await Podcast.loadFeed(
@@ -21,7 +24,7 @@ class PodcastService {
     PodcastGenre podcastGenre = PodcastGenre.science,
     Country country = Country.UNITED_KINGDOM,
   }) {
-    Search()
+    _search
         .charts(
       genre: podcastGenre.id,
       limit: 10,
@@ -63,5 +66,78 @@ class PodcastService {
         _chartsChangedController.add(true);
       }
     });
+  }
+
+  final _searchChangedController = StreamController<bool>.broadcast();
+  Stream<bool> get searchChanged => _chartsChangedController.stream;
+  Set<Set<Audio>>? _searchResult;
+  Set<Set<Audio>>? get searchResult => _searchResult;
+
+  void _updateSearchResult(Set<Set<Audio>>? value) {
+    _searchResult = value;
+    _searchChangedController.add(true);
+  }
+
+  Future<void> search({
+    String? searchQuery,
+    bool useAlbumImage = false,
+    PodcastGenre podcastGenre = PodcastGenre.science,
+    Country country = Country.UNITED_KINGDOM,
+    Language language = Language.NONE,
+  }) async {
+    if (searchQuery?.isEmpty == true) return;
+    _updateSearchResult(null);
+
+    SearchResult results = await _search.search(
+      searchQuery!,
+      country: country,
+      language: language,
+      limit: 10,
+    );
+
+    if (results.successful && results.items.isNotEmpty) {
+      _searchResult ??= {};
+      _searchResult?.clear();
+
+      for (var item in results.items) {
+        if (item.feedUrl != null) {
+          try {
+            final Podcast podcast = await compute(loadPodcast, item.feedUrl!);
+
+            if (podcast.episodes?.isNotEmpty == true) {
+              final episodes = <Audio>{};
+
+              for (var episode in podcast.episodes!) {
+                if (episode.contentUrl != null) {
+                  final audio = Audio(
+                    url: episode.contentUrl,
+                    audioType: AudioType.podcast,
+                    name: '${podcast.title ?? ''} - ${episode.title}',
+                    imageUrl: useAlbumImage
+                        ? podcast.image ?? episode.imageUrl
+                        : episode.imageUrl ?? podcast.image,
+                    metadata: Metadata(
+                      title: episode.title,
+                      album: podcast.title,
+                      artist: podcast.copyright,
+                    ),
+                    description: podcast.description,
+                    website: podcast.url,
+                  );
+
+                  episodes.add(audio);
+                }
+              }
+              _searchResult?.add(episodes);
+              _searchChangedController.add(true);
+            }
+          } on PodcastFailedException {
+            _searchChangedController.add(true);
+          }
+        }
+      }
+    } else {
+      _updateSearchResult({});
+    }
   }
 }
