fb0122b7f49da3a51bc0ae339fae8c8ee29483ce
Frederik Feichtmeier <frederik.feichtmeier@gmail.com>
2023-07-24 16:14:53 +0200

    Rework_players (#92)
    * Rework player view
    
    * improve queue popup

--- linux/my_application.cc
+++ linux/my_application.cc
@@ -26,7 +26,7 @@ static void my_application_activate(GApplication* application) {
   gtk_container_add(GTK_CONTAINER(window), GTK_WIDGET(box));
 
   GdkGeometry geometry_min;
-  geometry_min.min_width = 680;
+  geometry_min.min_width = 715;
   geometry_min.min_height = 600;
   gtk_window_set_geometry_hints(window, nullptr, &geometry_min, GDK_HINT_MIN_SIZE);
   gtk_window_set_default_size(window, 900, 800);
