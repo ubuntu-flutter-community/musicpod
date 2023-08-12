79ec633bcb3cf7a6a6066be8a4c99130d06ff106
Feichtmeier <frederik.feichtmeier@gmail.com>
2023-08-12 13:10:26 +0200

    Use Platform.localeName for DateFormat

--- lib/app/podcasts/podcast_audio_tile.dart
+++ lib/app/podcasts/podcast_audio_tile.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_html/flutter_html.dart';
 import 'package:intl/intl.dart';
@@ -44,7 +46,7 @@ class PodcastAudioTile extends StatelessWidget {
       fontSize: 16,
     );
 
-    final date = DateFormat.MMMEd(countryCode)
+    final date = DateFormat.MMMEd(Platform.localeName)
         .format(DateTime.fromMillisecondsSinceEpoch(audio.year!));
     final duration = formatTime(
       audio.durationMs != null
