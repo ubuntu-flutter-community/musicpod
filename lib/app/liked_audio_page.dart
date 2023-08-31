96143bf4dcc9f1ebd140b331c93a3a0e77ada26a
Feichtmeier <frederik.feichtmeier@gmail.com>
2023-08-31 11:57:19 +0200

    Split favs into 2 pages

--- lib/app/liked_audio_page.dart
+++ lib/app/liked_audio_page.dart
@@ -1,14 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:musicpod/app/common/audio_page.dart';
+import 'package:musicpod/app/common/audio_page_body.dart';
+import 'package:musicpod/constants.dart';
 import 'package:musicpod/data/audio.dart';
 import 'package:musicpod/l10n/l10n.dart';
 import 'package:yaru_icons/yaru_icons.dart';
+import 'package:yaru_widgets/yaru_widgets.dart';
 
 class LikedAudioPage extends StatelessWidget {
   const LikedAudioPage({
     super.key,
     this.onTextTap,
-    this.likedAudios,
+    this.likedLocalAudios,
+    this.likedPodcasts,
   });
 
   static Widget createIcon({
@@ -24,17 +28,52 @@ class LikedAudioPage extends StatelessWidget {
     required String text,
     required AudioType audioType,
   })? onTextTap;
-  final Set<Audio>? likedAudios;
+  final Set<Audio>? likedLocalAudios;
+  final Set<Audio>? likedPodcasts;
 
   @override
   Widget build(BuildContext context) {
-    return AudioPage(
+    final localFavs = AudioPageBody(
+      key: ValueKey(likedLocalAudios),
+      pageId: 'likedAudio',
+      audios: likedLocalAudios,
       onTextTap: onTextTap,
       audioPageType: AudioPageType.likedAudio,
-      showAudioPageHeader: false,
-      audios: likedAudios,
+    );
+
+    final podcastFavs = AudioPageBody(
+      showAudioTileHeader: false,
+      key: ValueKey(likedPodcasts),
       pageId: 'likedAudio',
       headerTitle: context.l10n.likedSongs,
+      audios: likedPodcasts,
+      onTextTap: onTextTap,
+      audioPageType: AudioPageType.likedAudio,
+    );
+
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+        appBar: YaruWindowTitleBar(
+          title: SizedBox(
+            width: kSearchBarWidth,
+            child: TabBar(
+              tabs: [
+                Tab(text: context.l10n.localAudio),
+                Tab(text: context.l10n.podcasts),
+              ],
+            ),
+          ),
+          border: BorderSide.none,
+          backgroundColor: Colors.transparent,
+        ),
+        body: TabBarView(
+          children: [
+            localFavs,
+            podcastFavs,
+          ],
+        ),
+      ),
     );
   }
 }
