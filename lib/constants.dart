7b7b8cf424098fab31b64aa5ed531472e139eadc
Frederik Feichtmeier <frederik.feichtmeier@gmail.com>
2023-11-03 20:23:42 +0100

    Radio icy tags spotify, youtubemusic and apple music search links (#222)
    * add spotify search
    
    * Improve tooltips
    
    * add apple button

--- lib/constants.dart
+++ lib/constants.dart
@@ -5,9 +5,9 @@ const kRepoUrl = 'http://github.com/ubuntu-flutter-community/musicpod';
 
 const kSearchBarWidth = 350.0;
 
-const kSnackBarWidth = 450.0;
+const kSnackBarWidth = 500.0;
 
-const kSnackBarDuration = Duration(seconds: 5);
+const kSnackBarDuration = Duration(seconds: 10);
 
 const kGridPadding = EdgeInsets.only(
   bottom: kYaruPagePadding,
--- lib/src/common/copy_clipboard_content.dart
+++ lib/src/common/copy_clipboard_content.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_tabler_icons/flutter_tabler_icons.dart';
 import 'package:url_launcher/url_launcher.dart';
 import 'package:yaru_icons/yaru_icons.dart';
 
@@ -16,6 +17,60 @@ class CopyClipboardContent extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    String? artist;
+    String? title;
+    if (text != null) {
+      final split = text!.split('-');
+
+      if (split.length == 2) {
+        artist = split.first.replaceAll(' ', '%20').trim();
+        title = split.last.trim();
+      }
+    }
+
+    final spotifyButton = IconButton(
+      tooltip: 'Spotify ${context.l10n.search}',
+      onPressed: () => launchUrl(
+        Uri.parse(
+          'https://open.spotify.com/search/artist:$artist%20track:$title',
+        ),
+      ),
+      icon: Icon(
+        TablerIcons.brand_spotify,
+        color: Colors.white.withOpacity(0.9),
+      ),
+    );
+
+    //https://music.apple.com/us/search?term=
+    final appleButton = IconButton(
+      tooltip: 'Spotify ${context.l10n.search}',
+      onPressed: () => launchUrl(
+        Uri.parse(
+          'https://music.apple.com/us/search?term=$text',
+        ),
+      ),
+      icon: Icon(
+        TablerIcons.brand_apple,
+        color: Colors.white.withOpacity(0.9),
+      ),
+    );
+
+    final searchButton = IconButton(
+      tooltip: onSearch != null
+          ? context.l10n.search
+          : 'YouTubeMusic ${context.l10n.search}',
+      onPressed: onSearch ??
+          () => launchUrl(
+                Uri.parse(
+                  'https://music.youtube.com/search?q=${text!}',
+                ),
+              ),
+      icon: Icon(
+        onSearch != null ? YaruIcons.globe : TablerIcons.brand_youtube,
+        color: Colors.white.withOpacity(0.9),
+      ),
+    );
+
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 10),
       child: Row(
@@ -39,18 +94,13 @@ class CopyClipboardContent extends StatelessWidget {
               ],
             ),
           ),
-          IconButton(
-            tooltip: context.l10n.search,
-            onPressed: onSearch ??
-                () => launchUrl(
-                      Uri.parse(
-                        'https://music.youtube.com/search?q=${text!}',
-                      ),
-                    ),
-            icon: Icon(
-              YaruIcons.globe,
-              color: Colors.white.withOpacity(0.9),
-            ),
+          Row(
+            children: [
+              if (artist?.isNotEmpty == true && title?.isNotEmpty == true)
+                spotifyButton,
+              searchButton,
+              appleButton,
+            ],
           ),
         ],
       ),
