5e56c735b6e6d4a94766720e808ce62415404377
Feichtmeier <frederik.feichtmeier@gmail.com>
2023-11-06 21:23:38 +0100

    no scaffold fullscreen bg

--- lib/src/app/app.dart
+++ lib/src/app/app.dart
@@ -324,7 +324,6 @@ class _AppState extends State<App> {
           ),
           if (isFullScreen == true)
             Scaffold(
-              backgroundColor: playerBg,
               body: Material(
                 color: playerBg,
                 child: PlayerView(
