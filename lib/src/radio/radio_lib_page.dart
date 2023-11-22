26a4e7a2ceb2dfe020f83d933ee60ad2c486e3a1
Feichtmeier <frederik.feichtmeier@gmai.com>
2023-11-22 10:16:48 +0100

    Show empty radio lib message

--- lib/src/radio/radio_lib_page.dart
+++ lib/src/radio/radio_lib_page.dart
@@ -3,6 +3,7 @@ import 'package:provider/provider.dart';
 
 import '../../common.dart';
 import '../../data.dart';
+import '../../l10n.dart';
 import '../../library.dart';
 import 'station_card.dart';
 
@@ -35,22 +36,27 @@ class RadioLibPage extends StatelessWidget {
 
     return Padding(
       padding: const EdgeInsets.only(top: 15),
-      child: GridView.builder(
-        padding: gridPadding,
-        gridDelegate: imageGridDelegate,
-        itemCount: length,
-        itemBuilder: (context, index) {
-          final station = stations.entries.elementAt(index).value.firstOrNull;
-          return StationCard(
-            station: station,
-            play: play,
-            isStarredStation: isStarredStation,
-            onTextTap: onTextTap,
-            unstarStation: unstarStation,
-            starStation: starStation,
-          );
-        },
-      ),
+      child: length == 0
+          ? NoSearchResultPage(
+              message: Text(context.l10n.noStarredStations),
+            )
+          : GridView.builder(
+              padding: gridPadding,
+              gridDelegate: imageGridDelegate,
+              itemCount: length,
+              itemBuilder: (context, index) {
+                final station =
+                    stations.entries.elementAt(index).value.firstOrNull;
+                return StationCard(
+                  station: station,
+                  play: play,
+                  isStarredStation: isStarredStation,
+                  onTextTap: onTextTap,
+                  unstarStation: unstarStation,
+                  starStation: starStation,
+                );
+              },
+            ),
     );
   }
 }
