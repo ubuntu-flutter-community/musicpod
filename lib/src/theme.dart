3bafcc39a4921af2a35189ac28ecae3b40dc2963
Feichtmeier <frederik.feichtmeier@gmai.com>
2023-12-04 12:24:20 +0100

    re-use yaru menus for material because material menus are so incredibly ugly

--- lib/src/theme.dart
+++ lib/src/theme.dart
@@ -9,6 +9,11 @@ ThemeData m3Theme({
   final dividerColor = brightness == Brightness.light
       ? const Color.fromARGB(48, 0, 0, 0)
       : const Color.fromARGB(18, 255, 255, 255);
+  final colorScheme = ColorScheme.fromSeed(
+    surfaceTint: Colors.transparent,
+    seedColor: color,
+    brightness: brightness,
+  );
   return ThemeData(
     useMaterial3: true,
     dividerColor: dividerColor,
@@ -17,11 +22,10 @@ ThemeData m3Theme({
       space: 1.0,
       thickness: 0.0,
     ),
-    colorScheme: ColorScheme.fromSeed(
-      surfaceTint: Colors.transparent,
-      seedColor: color,
-      brightness: brightness,
-    ),
+    menuTheme: _createMenuTheme(colorScheme),
+    popupMenuTheme: _createPopupMenuTheme(colorScheme),
+    dropdownMenuTheme: _createDropdownMenuTheme(colorScheme),
+    colorScheme: colorScheme,
     splashFactory:
         Platform.isMacOS || Platform.isIOS ? NoSplash.splashFactory : null,
   );
@@ -50,3 +54,65 @@ Color getPlayerBg(Color? surfaceTintColor, Color scaffoldBg) {
 bool get yaruStyled => Platform.isLinux;
 
 bool get appleStyled => Platform.isMacOS || Platform.isIOS;
+
+PopupMenuThemeData _createPopupMenuTheme(ColorScheme colorScheme) {
+  final bgColor = colorScheme.brightness == Brightness.light
+      ? colorScheme.surface
+      : colorScheme.surfaceVariant;
+  return PopupMenuThemeData(
+    color: bgColor,
+    surfaceTintColor: bgColor,
+    shape: OutlineInputBorder(
+      borderRadius: BorderRadius.circular(10),
+      borderSide: BorderSide(
+        color: colorScheme.onSurface.withOpacity(
+          colorScheme.brightness == Brightness.light ? 0.3 : 0.2,
+        ),
+        width: 1,
+      ),
+    ),
+  );
+}
+
+MenuStyle _createMenuStyle(ColorScheme colorScheme) {
+  final bgColor = colorScheme.brightness == Brightness.light
+      ? colorScheme.surface
+      : colorScheme.surfaceVariant;
+
+  return MenuStyle(
+    surfaceTintColor: MaterialStateColor.resolveWith((states) => bgColor),
+    shape: MaterialStateProperty.resolveWith(
+      (states) => RoundedRectangleBorder(
+        side: BorderSide(
+          color: colorScheme.onSurface.withOpacity(
+            colorScheme.brightness == Brightness.light ? 0.3 : 0.2,
+          ),
+          width: 1,
+        ),
+        borderRadius: BorderRadius.circular(8),
+      ),
+    ),
+    side: MaterialStateBorderSide.resolveWith(
+      (states) => BorderSide(
+        color: colorScheme.onSurface.withOpacity(
+          colorScheme.brightness == Brightness.light ? 0.3 : 0.2,
+        ),
+        width: 1,
+      ),
+    ),
+    elevation: MaterialStateProperty.resolveWith((states) => 1),
+    backgroundColor: MaterialStateProperty.resolveWith((states) => bgColor),
+  );
+}
+
+MenuThemeData _createMenuTheme(ColorScheme colorScheme) {
+  return MenuThemeData(
+    style: _createMenuStyle(colorScheme),
+  );
+}
+
+DropdownMenuThemeData _createDropdownMenuTheme(ColorScheme colorScheme) {
+  return DropdownMenuThemeData(
+    menuStyle: _createMenuStyle(colorScheme),
+  );
+}
