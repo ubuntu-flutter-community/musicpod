97d1bf66cb4d79b0c68efe7934a315f74a7f3a52
Feichtmeier <frederik.feichtmeier@gmail.com>
2024-02-26 10:55:42 +0100

    re-tag test.mp3

--- test/local_audio_service_test.dart
+++ test/local_audio_service_test.dart
@@ -1,5 +1,6 @@
 import 'dart:io';
 
+import 'package:collection/collection.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/annotations.dart';
@@ -10,46 +11,50 @@ import 'package:musicpod/settings.dart';
 import 'local_audio_service_test.mocks.dart';
 
 @GenerateMocks([SettingsService])
-const Audio testMp3 = Audio(
-  title: 'test',
-  artist: 'musicpod',
-  genre: 'Black Metal',
-  album: 'podmusic',
-);
-
-const Audio testOgg = Audio(
-  title: 'Cold Stones',
-  artist: 'Backslash Zero',
-  album: 'DEMO2017',
-);
-
 Future<void> main() async {
+  const Audio testMp3 = Audio(
+    title: 'test',
+    artist: 'musicpod',
+    genre: 'Black Metal',
+    album: 'podmusic',
+  );
+
+  const Audio testOgg = Audio(
+    title: 'Cold Stones',
+    artist: 'Backslash Zero',
+    album: 'DEMO2017',
+    genre: 'Rock',
+  );
+
   WidgetsFlutterBinding.ensureInitialized();
   final service = LocalAudioService(
     settingsService: MockSettingsService(),
   );
   await service.init(testDir: Directory.current.path);
 
-  test('testTestMp3', () {
-    final audios = service.audios;
-    expect(audios?.isNotEmpty, true);
+  group('test metadata', () {
+    test('testTestMp3', () {
+      final audios = service.audios;
+      expect(audios?.isNotEmpty, true);
 
-    final testTestMp3 =
-        audios?.firstWhere((e) => e.path?.contains('test.mp3') == true);
-    expect(audios?.first.title, testTestMp3?.title);
-    expect(audios?.first.artist, testTestMp3?.artist);
-    expect(audios?.first.genre, testTestMp3?.genre);
-    expect(audios?.first.album, testTestMp3?.album);
-  });
+      final testTestMp3 =
+          audios?.firstWhereOrNull((e) => e.path?.contains('test.mp3') == true);
+      expect(testTestMp3?.title, testMp3.title);
+      expect(testTestMp3?.artist, testMp3.artist);
+      expect(testTestMp3?.album, testMp3.album);
+      expect(testTestMp3?.genre, testMp3.genre);
+    });
 
-  test('testTestOgg', () {
-    final audios = service.audios;
-    expect(audios?.isNotEmpty, true);
+    test('testTestOgg', () {
+      final audios = service.audios;
+      expect(audios?.isNotEmpty, true);
 
-    final coldStones =
-        audios?.firstWhere((e) => e.path?.contains('Backslash Zero') == true);
-    expect(audios?.last.title, coldStones?.title);
-    expect(audios?.last.artist, coldStones?.artist);
-    expect(audios?.last.album, coldStones?.album);
+      final coldStones = audios
+          ?.firstWhereOrNull((e) => e.path?.contains('Backslash Zero') == true);
+      expect(coldStones?.title, testOgg.title);
+      expect(coldStones?.artist, testOgg.artist);
+      expect(coldStones?.album, testOgg.album);
+      expect(coldStones?.genre, testOgg.genre);
+    });
   });
 }
