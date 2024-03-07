f25811639300b75ba62a6bb2988e4728b9b88605
Frederik Feichtmeier <frederik.feichtmeier@gmail.com>
2024-03-08 00:37:58 +0100

    Fix radio push on empty collections (#557)

--- lib/src/radio/open_radio_discover_page_button.dart
+++ lib/src/radio/open_radio_discover_page_button.dart
@@ -1,10 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
 import '../../app.dart';
 import '../../common.dart';
 import '../../globals.dart';
 import '../../l10n.dart';
-import '../../library.dart';
-import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'radio_discover_page.dart';
 
 class OpenRadioDiscoverPageButton extends ConsumerWidget {
@@ -12,19 +12,16 @@ class OpenRadioDiscoverPageButton extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final libraryModel = ref.read(libraryModelProvider);
     final isOnline =
         ref.watch(appModelProvider.select((value) => value.isOnline));
     return ImportantButton(
       onPressed: () {
-        if (libraryModel.starredStations.isEmpty) {
-          navigatorKey.currentState?.push(
-            MaterialPageRoute(
-              builder: (context) =>
-                  isOnline ? const RadioDiscoverPage() : const OfflinePage(),
-            ),
-          );
-        }
+        navigatorKey.currentState?.push(
+          MaterialPageRoute(
+            builder: (context) =>
+                isOnline ? const RadioDiscoverPage() : const OfflinePage(),
+          ),
+        );
       },
       child: Text(context.l10n.discover),
     );
