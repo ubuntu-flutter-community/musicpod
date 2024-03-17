27333e2ed8c9675121b9381aa4ff38581de648a5
Feichtmeier <frederik.feichtmeier@gmail.com>
2024-03-17 10:12:42 +0100

    Release 1.3.0

--- lib/src/patch_notes/patch_notes.dart
+++ lib/src/patch_notes/patch_notes.dart
@@ -144,8 +144,20 @@ const kPatchNotes20240311 = 'Hello MusicPod Users 💙🐧🧡\n'
     '\n\nIf you like MusicPod please consider sponsoring me 🙏:'
     '\n';
 
-const kRecentPatchNotesDisposed = kPatchNotes20240311disposed;
-const kRecentPatchNotesTitle = kPatchNotesTitle20240311;
-const kRecentPatchNotes = kPatchNotes20240311;
+// 2024 03 17
+const kPatchNotes20240317disposed = 'patchNotes20240317disposed';
+const kPatchNotesTitle20240317 = 'Patch notes: 2024-03-17';
+const kPatchNotes20240317 = 'Hello MusicPod Users 💙🐧🧡\n'
+    '\n* New features:'
+    '\n* manual imports from the + button in the sidebar for'
+    '\n* playlists'
+    '\n* stations'
+    '\n* podcasts'
+    '\n\nIf you like MusicPod please consider sponsoring me 🙏:'
+    '\n';
+
+const kRecentPatchNotesDisposed = kPatchNotes20240317disposed;
+const kRecentPatchNotesTitle = kPatchNotesTitle20240317;
+const kRecentPatchNotes = kPatchNotes20240317;
 
 const kPatchNotesDisposed = 'kPatchNotesDisposed';
