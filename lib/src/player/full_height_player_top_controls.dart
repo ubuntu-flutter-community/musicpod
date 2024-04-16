7c5fae2ca1785a801ed895a4b38703c0ae76835f
Frederik Feichtmeier <frederik.feichtmeier@gmail.com>
2024-04-16 23:37:27 +0200

    fix: Radio page is one large block of colour (#658)
    * fix: Radio page is one large block of colour
    Fixes #657
    
    * Upgrade version

--- lib/src/player/full_height_player_top_controls.dart
+++ lib/src/player/full_height_player_top_controls.dart
@@ -1,4 +1,7 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:yaru/constants.dart';
 
 import '../../common.dart';
 import '../../data.dart';
@@ -14,6 +17,7 @@ class FullHeightPlayerTopControls extends StatelessWidget {
     required this.activeControls,
     required this.playerViewMode,
     required this.onFullScreenPressed,
+    required this.isVideo,
   });
 
   final Audio? audio;
@@ -21,47 +25,60 @@ class FullHeightPlayerTopControls extends StatelessWidget {
   final bool activeControls;
   final PlayerViewMode playerViewMode;
   final void Function() onFullScreenPressed;
+  final bool isVideo;
 
   @override
   Widget build(BuildContext context) {
-    return Wrap(
-      alignment: WrapAlignment.end,
-      spacing: 5.0,
-      children: [
-        if (audio?.audioType != AudioType.podcast)
-          PlayerLikeIcon(
-            audio: audio,
-            color: iconColor,
-          ),
-        QueueButton(
-          color: iconColor,
+    return Padding(
+      padding: EdgeInsets.only(
+        right: kYaruPagePadding,
+        top: Platform.isMacOS ? 0 : kYaruPagePadding,
+      ),
+      child: Container(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(100),
+          color: isVideo ? Colors.black.withOpacity(0.6) : null,
         ),
-        ShareButton(
-          audio: audio,
-          active: activeControls,
-          color: iconColor,
+        child: Wrap(
+          alignment: WrapAlignment.end,
+          spacing: 5.0,
+          children: [
+            if (audio?.audioType != AudioType.podcast)
+              PlayerLikeIcon(
+                audio: audio,
+                color: iconColor,
+              ),
+            QueueButton(
+              color: iconColor,
+            ),
+            ShareButton(
+              audio: audio,
+              active: activeControls,
+              color: iconColor,
+            ),
+            if (audio?.audioType == AudioType.podcast)
+              PlaybackRateButton(
+                active: activeControls,
+                color: iconColor,
+              ),
+            VolumeSliderPopup(
+              color: iconColor,
+            ),
+            IconButton(
+              tooltip: playerViewMode == PlayerViewMode.fullWindow
+                  ? context.l10n.leaveFullWindow
+                  : context.l10n.fullWindow,
+              icon: Icon(
+                playerViewMode == PlayerViewMode.fullWindow
+                    ? Iconz().fullScreenExit
+                    : Iconz().fullScreen,
+                color: iconColor,
+              ),
+              onPressed: onFullScreenPressed,
+            ),
+          ],
         ),
-        if (audio?.audioType == AudioType.podcast)
-          PlaybackRateButton(
-            active: activeControls,
-            color: iconColor,
-          ),
-        VolumeSliderPopup(
-          color: iconColor,
-        ),
-        IconButton(
-          tooltip: playerViewMode == PlayerViewMode.fullWindow
-              ? context.l10n.leaveFullWindow
-              : context.l10n.fullWindow,
-          icon: Icon(
-            playerViewMode == PlayerViewMode.fullWindow
-                ? Iconz().fullScreenExit
-                : Iconz().fullScreen,
-            color: iconColor,
-          ),
-          onPressed: onFullScreenPressed,
-        ),
-      ],
+      ),
     );
   }
 }
