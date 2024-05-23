1e170312bce14b6fe255e1396b3faa00d339fb47
Frederik Feichtmeier <frederik.feichtmeier@gmail.com>
2024-05-23 19:41:50 +0200

    dont show backbutton on sidebar player headerbar (#771)

--- lib/src/player/view/full_height_player.dart
+++ lib/src/player/view/full_height_player.dart
@@ -121,6 +121,7 @@ class FullHeightPlayer extends StatelessWidget with WatchItMixin {
 
     final headerBar = HeaderBar(
       adaptive: false,
+      includeBackButton: false,
       title: const Text(
         '',
         maxLines: 1,
