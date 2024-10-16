7b3c193208f1b4de1a457690043d0213ad4ffd38
Frederik Feichtmeier <frederik.feichtmeier@gmail.com>
2024-10-16 21:37:51 +0200

    chore: always allow discord for mac and windows (#958)

--- lib/app_config.dart
+++ lib/app_config.dart
@@ -1,6 +1,10 @@
-// TODO(#946): make discord work inside the snap
-// or leave it for linux disabled if this won't work
+import 'dart:io';
+
 import 'package:flutter/foundation.dart';
 
+// TODO(#946): make discord work inside the snap
+// or leave it for linux disabled if this won't work
 bool allowDiscordRPC = kDebugMode ||
+    Platform.isMacOS ||
+    Platform.isWindows ||
     bool.tryParse(const String.fromEnvironment('ALLOW_DISCORD_RPC')) == true;
