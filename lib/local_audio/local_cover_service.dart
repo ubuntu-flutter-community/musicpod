dd6512c9877e461fd0665e47c67d413650882536
Frederik Feichtmeier <frederik.feichtmeier@gmail.com>
2024-11-25 21:43:08 +0100

    feat: also use the cover image stored inside the album folders (#1049)
    Fixes #1048

--- lib/local_audio/local_cover_service.dart
+++ lib/local_audio/local_cover_service.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 import 'dart:io';
 import 'dart:typed_data';
+import 'package:path/path.dart' as p;
 
 import 'package:audio_metadata_reader/audio_metadata_reader.dart';
 import 'package:collection/collection.dart';
@@ -23,11 +24,23 @@ class LocalCoverService {
     final file = File(path);
     if (file.existsSync() && albumId.isNotEmpty == true) {
       final metadata = readMetadata(file, getImage: true);
+      var bytesFromMetadata =
+          metadata.pictures.firstWhereOrNull((e) => e.bytes.isNotEmpty)?.bytes;
+
+      if (bytesFromMetadata == null) {
+        final maybeImageInFolder = _commonCoverInFolderNames.firstWhereOrNull(
+          (e) => _maybeImageInFolderExists(file: file, suffix: e),
+        );
+        if (maybeImageInFolder != null) {
+          bytesFromMetadata = File(maybeImageInFolder).readAsBytesSync();
+        }
+      }
+
+      if (bytesFromMetadata == null) return null;
+
       final cover = _put(
         albumId: albumId,
-        data: metadata.pictures
-            .firstWhereOrNull((e) => e.bytes.isNotEmpty)
-            ?.bytes,
+        data: bytesFromMetadata,
       );
       if (cover != null) {
         _propertiesChangedController.add(true);
@@ -36,6 +49,14 @@ class LocalCoverService {
     }
     return null;
   }
+
+  static const _commonCoverInFolderNames = ['front', 'cover', 'album'];
+
+  bool _maybeImageInFolderExists({
+    required File file,
+    required String suffix,
+  }) =>
+      Directory(p.join(file.parent.path, suffix)).existsSync();
 
   Uint8List? _put({required String albumId, Uint8List? data}) {
     return _store.containsKey(albumId)
