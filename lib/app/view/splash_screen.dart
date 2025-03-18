d446f5d6c67a1eff582dd3020fa7890adca7cae8
Frederik Feichtmeier <frederik.feichtmeier@gmail.com>
2025-03-18 23:07:50 +0100

    fix: make dep registration sync (#1176)

--- lib/app/view/splash_screen.dart
+++ lib/app/view/splash_screen.dart
@@ -1,30 +1,49 @@
 import 'package:flutter/material.dart';
+import 'package:phoenix_theme/phoenix_theme.dart';
+import 'package:yaru/yaru.dart';
 
-import '../../common/view/header_bar.dart';
+import '../../app_config.dart';
 import '../../common/view/progress.dart';
+import '../../common/view/ui_constants.dart';
+import '../../l10n/l10n.dart';
 
 class SplashScreen extends StatelessWidget {
   const SplashScreen({super.key});
 
   @override
-  Widget build(BuildContext context) => Scaffold(
-        appBar: const HeaderBar(adaptive: false, title: Text('')),
-        body: Center(
-          child: SingleChildScrollView(
-            child: Center(
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  Image.asset(
-                    'assets/icon.png',
-                    height: 250,
-                    width: 250,
-                  ),
-                  const Padding(
-                    padding: EdgeInsets.all(20),
-                    child: Progress(),
-                  ),
-                ],
+  Widget build(BuildContext context) => MaterialApp(
+        theme: isGtkApp
+            ? yaruLight
+            : phoenixTheme(color: kMusicPodDefaultColor).lightTheme,
+        darkTheme: isGtkApp
+            ? yaruDark
+            : phoenixTheme(color: kMusicPodDefaultColor).darkTheme,
+        title: '',
+        debugShowCheckedModeBanner: false,
+        localizationsDelegates: AppLocalizations.localizationsDelegates,
+        supportedLocales: supportedLocales,
+        home: Scaffold(
+          appBar: const YaruWindowTitleBar(
+            border: BorderSide.none,
+            backgroundColor: Colors.transparent,
+          ),
+          body: Center(
+            child: SingleChildScrollView(
+              child: Center(
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Image.asset(
+                      'assets/icon.png',
+                      height: 250,
+                      width: 250,
+                    ),
+                    const Padding(
+                      padding: EdgeInsets.all(20),
+                      child: Progress(),
+                    ),
+                  ],
+                ),
               ),
             ),
           ),
