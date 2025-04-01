4b57fefaae61c685c222abccf226cde101cb44d5
Feichtmeier <frederik.feichtmeier@gmail.com>
2025-04-01 23:00:28 +0200

    feat: persist window size
    Fixes #626

--- lib/register.dart
+++ lib/register.dart
@@ -1,4 +1,5 @@
 import 'dart:io';
+import 'dart:ui';
 
 import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:desktop_notifications/desktop_notifications.dart';
@@ -11,13 +12,16 @@ import 'package:media_kit_video/media_kit_video.dart';
 import 'package:package_info_plus/package_info_plus.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:watch_it/watch_it.dart';
+import 'package:window_manager/window_manager.dart';
 
 import 'app/app_model.dart';
 import 'app/connectivity_model.dart';
+import 'app/window_size_to_settings_listener.dart';
 import 'app_config.dart';
 import 'expose/expose_service.dart';
 import 'expose/lastfm_service.dart';
 import 'expose/listenbrainz_service.dart';
+import 'extensions/shared_preferences_x.dart';
 import 'external_path/external_path_service.dart';
 import 'library/library_model.dart';
 import 'library/library_service.dart';
@@ -55,8 +59,39 @@ void registerDependencies({required List<String> args}) async {
     );
   }
 
+  // TODO: try to register window manager inside get_it
   di
-    ..registerSingletonAsync<SharedPreferences>(SharedPreferences.getInstance)
+    ..registerSingletonAsync<SharedPreferences>(() async {
+      final prefs = await SharedPreferences.getInstance();
+      final wm = WindowManager.instance;
+      wm.addListener(
+        WindowSizeToSettingsListener(
+          onFullscreen: (v) => prefs.setBool(SPKeys.windowFullscreen, v),
+          onMaximize: (v) => prefs.setBool(SPKeys.windowMaximized, v),
+          onResize: (v) =>
+              prefs.setInt(SPKeys.windowHeight, v.height.toInt()).then(
+                    (_) => prefs.setInt(SPKeys.windowWidth, v.width.toInt()),
+                  ),
+        ),
+      );
+
+      if (prefs.getBool(SPKeys.windowFullscreen) ?? false) {
+        WindowManager.instance.setFullScreen(true);
+      } else if (prefs.getBool(SPKeys.windowMaximized) ?? false) {
+        WindowManager.instance.maximize();
+      } else {
+        final height = prefs.getInt(SPKeys.windowHeight) ?? 820;
+        final width = prefs.getInt(SPKeys.windowWidth) ?? 950;
+        WindowManager.instance.setSize(
+          Size(
+            width.toDouble(),
+            height.toDouble(),
+          ),
+        );
+      }
+
+      return prefs;
+    })
     ..registerSingletonAsync<PackageInfo>(PackageInfo.fromPlatform)
     ..registerLazySingleton<Dio>(
       () => Dio(),
