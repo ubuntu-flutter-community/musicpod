cc1a4673aa978602442001437061c5c428c97863
Feichtmeier <frederik.feichtmeier@gmail.com>
2025-04-04 19:59:14 +0200

    build runner

--- test/local_audio_service_test.mocks.dart
+++ test/local_audio_service_test.mocks.dart
@@ -32,10 +32,12 @@ class MockLocalCoverService extends _i1.Mock implements _i2.LocalCoverService {
   }
 
   @override
-  _i3.Stream<bool> get propertiesChanged => (super.noSuchMethod(
-        Invocation.getter(#propertiesChanged),
-        returnValue: _i3.Stream<bool>.empty(),
-      ) as _i3.Stream<bool>);
+  _i3.Stream<bool> get propertiesChanged =>
+      (super.noSuchMethod(
+            Invocation.getter(#propertiesChanged),
+            returnValue: _i3.Stream<bool>.empty(),
+          )
+          as _i3.Stream<bool>);
 
   @override
   int get storeLength =>
@@ -46,16 +48,24 @@ class MockLocalCoverService extends _i1.Mock implements _i2.LocalCoverService {
   _i3.Future<_i4.Uint8List?> getCover({
     required String? albumId,
     required String? path,
+    dynamic Function()? onError,
   }) =>
       (super.noSuchMethod(
-        Invocation.method(#getCover, [], {#albumId: albumId, #path: path}),
-        returnValue: _i3.Future<_i4.Uint8List?>.value(),
-      ) as _i3.Future<_i4.Uint8List?>);
+            Invocation.method(#getCover, [], {
+              #albumId: albumId,
+              #path: path,
+              #onError: onError,
+            }),
+            returnValue: _i3.Future<_i4.Uint8List?>.value(),
+          )
+          as _i3.Future<_i4.Uint8List?>);
 
   @override
-  _i3.Future<void> dispose() => (super.noSuchMethod(
-        Invocation.method(#dispose, []),
-        returnValue: _i3.Future<void>.value(),
-        returnValueForMissingStub: _i3.Future<void>.value(),
-      ) as _i3.Future<void>);
+  _i3.Future<void> dispose() =>
+      (super.noSuchMethod(
+            Invocation.method(#dispose, []),
+            returnValue: _i3.Future<void>.value(),
+            returnValueForMissingStub: _i3.Future<void>.value(),
+          )
+          as _i3.Future<void>);
 }
