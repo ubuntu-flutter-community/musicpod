3765e8fe8697726017560c59b934c78dcf3dbe1b
Frederik Feichtmeier <frederik.feichtmeier@gmail.com>
2025-04-27 13:36:02 +0200

    fix: MusicPod not loading .opus files after the last update (#1232)
    Fixes #1220

--- lib/extensions/media_file_x.dart
+++ lib/extensions/media_file_x.dart
@@ -2,14 +2,42 @@ import 'dart:io';
 
 import 'package:mime/mime.dart';
 
+final _resolver = MimeTypeResolver();
+
 extension MediaFileX on File {
   bool get isPlayable => path.isPlayable;
 }
 
 extension _ValidPathX on String {
   bool get isPlayable {
-    final mime = lookupMimeType(this);
+    for (var v in _SpecialMimeTypes.values) {
+      _resolver
+        ..addExtension(v.extension, v.mimeType)
+        ..addMagicNumber(v.headerBytes, v.mimeType);
+    }
+
+    final mime = _resolver.lookup(this);
+
     return (mime?.contains('audio') ?? false) ||
         (mime?.contains('video') ?? false);
   }
+}
+
+enum _SpecialMimeTypes {
+  opusAudio;
+
+  String get mimeType => switch (this) { opusAudio => 'audio/opus' };
+
+  String get extension => switch (this) {
+        opusAudio => 'opus',
+      };
+
+  List<int> get headerBytes => switch (this) {
+        opusAudio => [
+            0x4F, // O
+            0x67, // g
+            0x67, // g
+            0x53, // S
+          ],
+      };
 }
