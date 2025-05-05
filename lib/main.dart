48771e41764b11ce63af4973fb708aca41ae6481
Frederik Feichtmeier <frederik.feichtmeier@gmail.com>
2025-05-05 23:45:59 +0200

    fix: load accent color on windows (#1275)

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:media_kit/media_kit.dart';
+import 'package:system_theme/system_theme.dart';
 import 'package:window_manager/window_manager.dart';
-import 'package:yaru/yaru.dart';
+import 'package:yaru/yaru.dart' show YaruWindowTitleBar;
 
 import 'app/view/musicpod.dart';
+import 'extensions/taget_platform_x.dart';
 import 'register.dart';
 
 Future<void> main(List<String> args) async {
@@ -15,6 +17,10 @@ Future<void> main(List<String> args) async {
     WindowManager.instance
       ..setMinimumSize(const Size(500, 700))
       ..setSize(const Size(950, 820));
+  }
+
+  if (isWindows) {
+    await SystemTheme.accentColor.load();
   }
 
   MediaKit.ensureInitialized();
