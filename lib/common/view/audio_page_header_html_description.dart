61dcd1f1eaca1852a0dd469a65f2a8c9edc05c7f
Feichtmeier <frederik.feichtmeier@gmail.com>
2025-09-24 19:19:19 +0200

    fix: exchange html widget library

--- lib/common/view/audio_page_header_html_description.dart
+++ lib/common/view/audio_page_header_html_description.dart
@@ -1,11 +1,10 @@
-import 'ui_constants.dart';
-import '../../extensions/build_context_x.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_html/flutter_html.dart';
-import 'package:url_launcher/url_launcher.dart';
 import 'package:yaru/yaru.dart';
 
+import '../../extensions/build_context_x.dart';
 import '../../extensions/theme_data_x.dart';
+import 'html_text.dart';
+import 'ui_constants.dart';
 
 class AudioPageHeaderHtmlDescription extends StatelessWidget {
   const AudioPageHeaderHtmlDescription({
@@ -38,27 +37,7 @@ class AudioPageHeaderHtmlDescription extends StatelessWidget {
               descriptionStyle: descriptionStyle,
             ),
           ),
-          child: Html(
-            data: description,
-            onAnchorTap: (url, attributes, element) {
-              if (url == null) return;
-              launchUrl(Uri.parse(url));
-            },
-            style: {
-              'img': Style(display: Display.none),
-              'body': Style(
-                height: Height.auto(),
-                margin: Margins.zero,
-                padding: HtmlPaddings.zero,
-                textOverflow: TextOverflow.ellipsis,
-                maxLines: 4,
-                textAlign: TextAlign.center,
-                fontSize: FontSize(descriptionStyle?.fontSize ?? 10),
-                fontWeight: descriptionStyle?.fontWeight,
-                fontFamily: descriptionStyle?.fontFamily,
-              ),
-            },
-          ),
+          child: HtmlText(text: description, wrapInFakeScroll: true),
         ),
       ),
     );
@@ -89,24 +68,12 @@ class _HtmlDialog extends StatelessWidget {
       children: [
         SizedBox(
           width: 400,
-          child: Html(
-            data: description,
-            onAnchorTap: (url, attributes, element) {
-              if (url == null) return;
-              launchUrl(Uri.parse(url));
-            },
-            style: {
-              'img': Style(display: Display.none),
-              'body': Style(
-                height: Height.auto(),
-                textOverflow: TextOverflow.ellipsis,
-                maxLines: 400,
-                textAlign: TextAlign.center,
-                fontSize: FontSize(descriptionStyle?.fontSize ?? 10),
-                fontWeight: descriptionStyle?.fontWeight,
-                fontFamily: descriptionStyle?.fontFamily,
-              ),
-            },
+          child: Padding(
+            padding: const EdgeInsets.all(kLargestSpace),
+            child: HtmlText(
+              text: description ?? '',
+              color: context.colorScheme.onSurface,
+            ),
           ),
         ),
       ],
---
+++ lib/common/view/html_text.dart
@@ -0,0 +1,71 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_widget_from_html_core/flutter_widget_from_html_core.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+import '../../extensions/build_context_x.dart';
+
+class HtmlText extends StatelessWidget {
+  const HtmlText({
+    super.key,
+    required this.text,
+    this.color,
+    this.onTapUrl,
+    this.wrapInFakeScroll = false,
+  });
+
+  final String text;
+  final Color? color;
+  final FutureOr<bool> Function(String)? onTapUrl;
+  final bool wrapInFakeScroll;
+
+  @override
+  Widget build(BuildContext context) {
+    final theColor = color ?? context.colorScheme.onSurface;
+
+    final htmlWidget = HtmlWidget(
+      text,
+      onTapUrl:
+          onTapUrl ??
+          (url) async {
+            await launchUrl(Uri.parse(url));
+            return true;
+          },
+      textStyle: TextStyle(color: color),
+      customStylesBuilder: (element) {
+        return {
+          'color': '${theColor.toHexTriplet()}',
+          if (wrapInFakeScroll) ...{
+            'overflow': 'hidden',
+            'text-overflow': 'ellipsis',
+            'width': '100%',
+            'display': '-webkit-box',
+            '-webkit-line-clamp': '1',
+            'ine-clamp': '1',
+            '-webkit-box-orient': 'vertical',
+          },
+        };
+      },
+    );
+
+    if (!wrapInFakeScroll) {
+      return htmlWidget;
+    }
+
+    return ScrollConfiguration(
+      behavior: ScrollConfiguration.of(context).copyWith(scrollbars: false),
+      child: SingleChildScrollView(
+        physics: const NeverScrollableScrollPhysics(),
+
+        child: htmlWidget,
+      ),
+    );
+  }
+}
+
+extension ColorX on Color {
+  String toHexTriplet() =>
+      // ignore: deprecated_member_use
+      '#${(value & 0xFFFFFF).toRadixString(16).padLeft(6, '0').toUpperCase()}';
+}
