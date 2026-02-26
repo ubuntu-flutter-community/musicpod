63b9d90848bfb8e7a4d4af92d0642cf8d5d231fe
Frederik Feichtmeier <frederik.feichtmeier@gmail.com>
2026-02-26 21:22:47 +0100

    fix: Entering and exiting full window mode resets scroll position to top of current tab (#1435)
    Fixes #1420

--- lib/player/view/bottom_player_image.dart
+++ lib/player/view/bottom_player_image.dart
@@ -12,7 +12,7 @@ import '../player_model.dart';
 import 'player_fall_back_image.dart';
 import 'player_remote_source_image.dart';
 
-class BottomPlayerImage extends StatelessWidget with WatchItMixin {
+class BottomPlayerImage extends StatefulWidget with WatchItStatefulWidgetMixin {
   const BottomPlayerImage({
     super.key,
     this.audio,
@@ -28,18 +28,25 @@ class BottomPlayerImage extends StatelessWidget with WatchItMixin {
   final bool isOnline;
 
   @override
+  State<BottomPlayerImage> createState() => _BottomPlayerImageState();
+}
+
+class _BottomPlayerImageState extends State<BottomPlayerImage> {
+  bool _hovered = false;
+
+  @override
   Widget build(BuildContext context) {
-    if (isVideo == true) {
+    if (widget.isVideo == true) {
       return RepaintBoundary(
         child: MouseRegion(
           cursor: SystemMouseCursors.click,
           child: GestureDetector(
             onTap: () => di<AppModel>().setFullWindowMode(true),
             child: Video(
-              height: size,
-              width: size,
+              height: widget.size,
+              width: widget.size,
               filterQuality: FilterQuality.medium,
-              controller: videoController,
+              controller: widget.videoController,
               controls: (state) {
                 return const SizedBox.shrink();
               },
@@ -52,56 +59,72 @@ class BottomPlayerImage extends StatelessWidget with WatchItMixin {
     Widget child;
 
     final fallBackImage = PlayerFallBackImage(
-      audioType: audio?.audioType,
-      height: size,
-      width: size,
+      audioType: widget.audio?.audioType,
+      height: widget.size,
+      width: widget.size,
     );
 
-    if (audio?.canHaveLocalCover == true) {
+    if (widget.audio?.canHaveLocalCover == true) {
       child = LocalCover(
-        key: ValueKey(audio!.path),
-        albumId: audio!.albumId!,
-        path: audio!.path!,
+        key: ValueKey(widget.audio!.path),
+        albumId: widget.audio!.albumId!,
+        path: widget.audio!.path!,
         fit: BoxFit.cover,
-        dimension: size,
+        dimension: widget.size,
         fallback: fallBackImage,
       );
     } else {
       child = PlayerRemoteSourceImage(
-        height: size,
-        width: size,
+        height: widget.size,
+        width: widget.size,
         fit: BoxFit.cover,
         fallBackIcon: fallBackImage,
         errorIcon: fallBackImage,
       );
     }
 
-    return Stack(
-      children: [
-        AnimatedSwitcher(
-          duration: const Duration(milliseconds: 200),
-          child: child,
-        ),
-        if (watchStream((PlayerModel s) => s.onlineArtError).data != null &&
-            audio?.audioType == AudioType.radio)
-          Positioned(
-            bottom: 5,
-            right: 5,
-            child: Padding(
-              padding: const EdgeInsets.only(left: 5),
-              child: Tooltip(
-                message: context.l10n.onlineArtError,
-                child: Icon(
-                  Iconz.imageMissingFilled,
-                  color: Colors.white,
-                  shadows: [
-                    const BoxShadow(offset: Offset(0, 1), blurRadius: 1),
-                  ],
+    return MouseRegion(
+      onEnter: (_) => setState(() => _hovered = true),
+      onExit: (_) => setState(() => _hovered = false),
+      child: Stack(
+        children: [
+          AnimatedSwitcher(
+            duration: const Duration(milliseconds: 200),
+            child: child,
+          ),
+          if (watchStream((PlayerModel s) => s.onlineArtError).data != null &&
+              widget.audio?.audioType == AudioType.radio)
+            Positioned(
+              bottom: 5,
+              right: 5,
+              child: Padding(
+                padding: const EdgeInsets.only(left: 5),
+                child: Tooltip(
+                  message: context.l10n.onlineArtError,
+                  child: Icon(
+                    Iconz.imageMissingFilled,
+                    color: Colors.white,
+                    shadows: [
+                      const BoxShadow(offset: Offset(0, 1), blurRadius: 1),
+                    ],
+                  ),
                 ),
               ),
             ),
-          ),
-      ],
+          if (_hovered && widget.isOnline)
+            Positioned.fill(
+              child: Container(
+                color: Colors.black45,
+                child: Center(
+                  child: IconButton(
+                    onPressed: () => di<AppModel>().setFullWindowMode(true),
+                    icon: Icon(Iconz.fullScreen, color: Colors.white),
+                  ),
+                ),
+              ),
+            ),
+        ],
+      ),
     );
   }
 }
