12339c7ef038103b88cf0b3e120c550e74466cfc
Feichtmeier <frederik.feichtmeier@gmail.com>
2026-06-04 18:48:06 +0200

    disable mpris setting for working snap in edge

--- lib/third_party/audio_service_module.dart
+++ lib/third_party/audio_service_module.dart
@@ -1,11 +1,9 @@
 import 'package:audio_service/audio_service.dart';
-import 'package:audio_service_mpris/audio_service_mpris.dart';
 import 'package:injectable/injectable.dart';
 
 import '../app/app_config.dart';
 import '../common/data/audio.dart';
 import '../common/data/audio_type.dart';
-import '../common/snap_detector.dart';
 import '../extensions/taget_platform_x.dart';
 import '../player/player_service.dart';
 
@@ -17,17 +15,17 @@ abstract class AudioServiceModule {
   ) async {
     final handler = await _registerAudioServiceHandler(playerService);
 
-    final mpris = Mpris();
-    mpris.identity = AppConfig.appTitle;
-    mpris.desktopEntry = SnapDetector.isSnap
-        ? AppConfig.snapDesktopEntry
-        : AppConfig.desktopEntry;
-    mpris.canGoNext = true;
-    mpris.canGoPrevious = true;
-    mpris.canPlay = true;
-    mpris.canPause = true;
-    mpris.canControl = true;
-    mpris.canRaise = true;
+    // final mpris = Mpris();
+    // mpris.identity = AppConfig.appTitle;
+    // mpris.desktopEntry = SnapDetector.isSnap
+    //     ? AppConfig.snapDesktopEntry
+    //     : AppConfig.desktopEntry;
+    // mpris.canGoNext = true;
+    // mpris.canGoPrevious = true;
+    // mpris.canPlay = true;
+    // mpris.canPause = true;
+    // mpris.canControl = true;
+    // mpris.canRaise = true;
 
     return handler;
   }
