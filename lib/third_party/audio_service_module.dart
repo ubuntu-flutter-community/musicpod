293ebce9854c178346f7c57bfff9f20251c42fef
Feichtmeier <frederik.feichtmeier@gmail.com>
2026-06-05 00:26:50 +0200

    revert back to old mpris since could get it to work

--- lib/third_party/audio_service_module.dart
+++ lib/third_party/audio_service_module.dart
@@ -1,12 +1,10 @@
 import 'package:audio_service/audio_service.dart';
-import 'package:audio_service_mpris/audio_service_mpris.dart';
 import 'package:injectable/injectable.dart';
 import 'package:window_manager/window_manager.dart';
 
 import '../app/app_config.dart';
 import '../common/data/audio.dart';
 import '../common/data/audio_type.dart';
-import '../common/snap_detector.dart';
 import '../extensions/taget_platform_x.dart';
 import '../player/player_service.dart';
 
@@ -17,23 +15,6 @@ abstract class AudioServiceModule {
     PlayerService playerService,
     WindowManager windowManager,
   ) async {
-    if (isLinux) {
-      await AudioServiceMpris.init(
-        dBusName: AppConfig.appId,
-        identity: AppConfig.appTitle,
-        desktopEntry: SnapDetector.isSnap
-            ? AppConfig.snapDesktopEntry
-            : AppConfig.desktopEntry,
-        canGoNext: true,
-        canGoPrevious: true,
-        canPlay: true,
-        canPause: true,
-        canControl: true,
-        enableLogging: false,
-        onRaiseRequest: windowManager.show,
-      );
-    }
-
     final handler = await _registerAudioServiceHandler(playerService);
 
     return handler;
