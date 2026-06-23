8ffb65bf386139f251cca308940da0d5e9a42092
Feichtmeier <frederik.feichtmeier@gmail.com>
2026-06-23 21:59:39 +0200

    chore: always auto dispose

--- lib/common/keep_alive_registry.dart
+++ lib/common/keep_alive_registry.dart
@@ -5,27 +5,29 @@ import 'logging.dart';
 class KeepAliveRegistry<I, T> {
   final _instances = <I, T>{};
 
+  KeepAliveRegistry({Duration autoDisposeAllAfter = const Duration(hours: 1)}) {
+    Future.delayed(autoDisposeAllAfter, () => disposeAll());
+  }
+
   T getOrRegister({
     required I id,
     required T Function() factoryFunction,
-    Duration? autoDisposeAfter,
+    Duration autoDisposeAfter = const Duration(minutes: 5),
   }) {
-    if (autoDisposeAfter != null) {
-      Future.delayed(
-        autoDisposeAfter,
-        () => dispose(
-          id,
-          message: 'Instance auto-disposed after $autoDisposeAfter',
-        ),
-      );
-    }
+    Future.delayed(
+      autoDisposeAfter,
+      () => dispose(id, reason: 'Auto dispose after $autoDisposeAfter'),
+    );
+
     return _instances.putIfAbsent(id, () {
       Logger.i('Instance created for id: $id', tag: '$T');
       return factoryFunction();
     });
   }
 
-  T? dispose(I id, {String message = 'Instance disposed'}) {
+  T? dispose(I id, {String? reason}) {
+    final message =
+        '${reason != null ? '$reason. ' : ''}Instance reference for id: $id removed, will be garbage collected if no other references exist';
     if (!_instances.containsKey(id)) return null;
     final instance = _instances.remove(id);
     if (instance != null) {
@@ -34,8 +36,12 @@ class KeepAliveRegistry<I, T> {
     return instance;
   }
 
-  void disposeAll() {
+  void disposeAll({String? reason}) {
     _instances.clear();
-    Logger.i('All instances disposed', tag: '$T');
+
+    Logger.i(
+      '${reason != null ? '$reason. ' : ''}All instances of $T disposed',
+      tag: '$T',
+    );
   }
 }
