3ae520ea28f75c8e956e2d53a7197d29625c5127
Frederik Feichtmeier <frederik.feichtmeier@gmail.com>
2026-06-26 18:41:26 +0200

    chore: mobile work (#1617)

--- lib/player/view/full_window_player_body.dart
+++ lib/player/view/full_window_player_body.dart
@@ -3,8 +3,13 @@ import 'package:flutter_it/flutter_it.dart';
 
 import '../../common/view/ui_constants.dart';
 import '../../extensions/build_context_x.dart';
-import 'full_window_player_main_panel.dart';
+import '../../extensions/platform_x.dart';
+import 'full_window_player_image.dart';
 import 'player_explorer.dart';
+import 'player_main_controls.dart';
+import 'player_title_and_artist.dart';
+import 'player_track.dart';
+import 'player_view.dart';
 
 class FullWindowPlayerBody extends StatelessWidget with WatchItMixin {
   const FullWindowPlayerBody({super.key, required this.active});
@@ -18,17 +23,17 @@ class FullWindowPlayerBody extends StatelessWidget with WatchItMixin {
     final playerWithSidePanel = mediaQuerySize.width > 2 * kPlayerExplorerWidth;
 
     return playerWithSidePanel
-        ? Row(
-            mainAxisSize: MainAxisSize.min,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              SizedBox(
-                width: kPlayerExplorerWidth,
-                child: FullWindowPlayerMainPanel(active: active),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(bottom: 3 * kLargestSpace),
-                child: ConstrainedBox(
+        ? Padding(
+            padding: const EdgeInsets.only(bottom: 3 * kLargestSpace),
+            child: Row(
+              mainAxisSize: MainAxisSize.min,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const SizedBox(
+                  width: kPlayerExplorerWidth,
+                  child: FullWindowPlayerImage(),
+                ),
+                ConstrainedBox(
                   constraints: BoxConstraints(
                     maxWidth: kPlayerExplorerWidth,
                     maxHeight:
@@ -36,15 +41,58 @@ class FullWindowPlayerBody extends StatelessWidget with WatchItMixin {
                   ),
                   child: PlayerExplorer(
                     selectedColor: context.colorScheme.onSurface,
-                    includeImage: false,
+                  ),
+                ),
+              ],
+            ),
+          )
+        : Column(
+            children: [
+              if (isMobile) const SizedBox(height: 1 * kLargestSpace),
+              Expanded(
+                child: PlayerExplorer(
+                  selectedColor: context.colorScheme.onSurface,
+                  firstChild: Padding(
+                    padding: isMobile
+                        ? EdgeInsets.zero
+                        : const EdgeInsets.only(bottom: 2 * kLargestSpace),
+                    child: FullWindowPlayerImage(
+                      dimension: isMobile
+                          ? kFullWindowPlayerImageSize + 45
+                          : null,
+                    ),
                   ),
                 ),
               ),
+              if (isMobile) ...[
+                const Padding(
+                  padding: EdgeInsets.only(
+                    bottom: 1 * kLargestSpace,
+                    left: 2 * kLargestSpace,
+                    right: 2 * kLargestSpace,
+                  ),
+                  child: Row(
+                    children: [
+                      Expanded(
+                        child: PlayerTitleAndArtist(
+                          playerPosition: PlayerPosition.fullWindow,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                const Padding(
+                  padding: EdgeInsets.only(
+                    bottom: 3 * kLargestSpace,
+                    left: 2 * kLargestSpace,
+                    right: 2 * kLargestSpace,
+                  ),
+                  child: const PlayerTrack(),
+                ),
+                PlayerMainControls(active: active),
+                const SizedBox(height: 3 * kLargestSpace),
+              ],
             ],
-          )
-        : PlayerExplorer(
-            selectedColor: context.colorScheme.onSurface,
-            includeImage: true,
           );
   }
 }
