cbc5fd54d276c8e49f39c6fff7ce603ece30887c
Frederik Feichtmeier <frederik.feichtmeier@gmail.com>
2026-09-24 18:41:08 +0200

    fix: change db location to XDG standard and migrate for not-flatpak/other OS (#1689)
    Fixes #1682

--- lib/third_party/database_module.dart
+++ lib/third_party/database_module.dart
@@ -1,11 +1,68 @@
+import 'dart:io';
+
 import 'package:drift_flutter/drift_flutter.dart';
 import 'package:injectable/injectable.dart';
+import 'package:path/path.dart' as p;
+import 'package:path_provider/path_provider.dart';
 
 import '../app/app_config.dart';
+import '../common/logging.dart';
 import '../common/persistence/database.dart';
 
 @module
 abstract class DatabaseModule {
   @lazySingleton
-  Database get database => Database(driftDatabase(name: AppConfig.appId));
+  Database get database => Database(
+    driftDatabase(
+      name: AppConfig.appId,
+      native: DriftNativeOptions(
+        databaseDirectory: () async {
+          final supportDir = await getApplicationSupportDirectory();
+          await supportDir.create(recursive: true);
+
+          Directory docsDir;
+          try {
+            docsDir = await getApplicationDocumentsDirectory();
+          } catch (e, stackTrace) {
+            Logger.w(
+              'Application documents directory not found or inaccessible: $e, ${AppConfig.appName} is now using support directory anyways!',
+              tag: '$DatabaseModule',
+              trace: stackTrace,
+            );
+            return supportDir;
+          }
+
+          try {
+            for (final ext in ['', '-wal', '-shm']) {
+              final oldFile = File(
+                p.join(docsDir.path, '${AppConfig.appId}.sqlite$ext'),
+              );
+              final newFile = File(
+                p.join(supportDir.path, '${AppConfig.appId}.sqlite$ext'),
+              );
+              if (await oldFile.exists() && !await newFile.exists()) {
+                Logger.i(
+                  'Found old database file at ${oldFile.path}, moving to ${newFile.path}',
+                  tag: '$DatabaseModule',
+                );
+                try {
+                  await oldFile.rename(newFile.path);
+                } on FileSystemException {
+                  await oldFile.copy(newFile.path);
+                  await oldFile.delete();
+                }
+              }
+            }
+          } catch (e, stackTrace) {
+            Logger.w(
+              'Failed to migrate database files: $e, ${AppConfig.appName} is now using support directory anyways!',
+              tag: '$DatabaseModule',
+              trace: stackTrace,
+            );
+          }
+          return supportDir;
+        },
+      ),
+    ),
+  );
 }
